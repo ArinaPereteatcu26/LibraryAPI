5d3613dd82bd8a71aaa75f956ff5f5d8c27dbbb3
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-08-28 11:12:58 +0300

    Add Library Solution with WebAPI project and DataAccess project

---
+++ LibraryWebAPI/LibraryDataAccess/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using LibraryDataAccess.Models;
+
+namespace LibraryDataAccess.NewFolder
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
---
+++ LibraryWebAPI/LibraryDataAccess/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using LibraryDataAccess.NewFolder;
+
+namespace LibraryDataAccess.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
